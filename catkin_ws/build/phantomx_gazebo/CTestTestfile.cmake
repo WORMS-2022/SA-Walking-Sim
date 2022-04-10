5cfe7e63f52faa7724fac374fcf486dd8f4d9b74
Juan Salazar <salazarj@mit.edu>
2022-04-10 10:17:15 -0700

    deleted build and devel folders

--- catkin_ws/build/phantomx_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aleks/catkin_ws/src/phantomx_gazebo
-# Build directory: /home/aleks/catkin_ws/build/phantomx_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
